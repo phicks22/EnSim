0f380193cbbf9ef4a9ef63b4a4e0490fb896603b
phicks22 <65268541+phicks22@users.noreply.github.com>
2021-10-10 13:49:32 -0700

    Test

---
+++ src/renderer/Display.java
@@ -0,0 +1,104 @@
+package renderer;
+
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+
+public class Display extends Canvas implements Runnable {
+    private static final long serialVersionUID = 1L;
+
+    private Thread thread;
+    private JFrame frame;
+    private static String title = "EnSim";
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private static boolean running = false;
+
+    public Display() {
+        this.frame = new JFrame();
+
+        Dimension size = new Dimension(WIDTH, HEIGHT);
+        this.setPreferredSize(size);
+
+    }
+
+    public static void main(String[] args){
+        Display display = new Display();
+        display.frame.setTitle(title);
+        display.frame.add(display);
+        display.frame.pack();
+        display.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        display.frame.setLocationRelativeTo(null);
+        display.frame.setResizable(false);
+        display.frame.setVisible(true);
+
+        display.start();
+    }
+
+    public synchronized void start(){
+        running = true;
+        this.thread = new Thread(this, "Display");
+        this.thread.start();
+    }
+
+    public synchronized void stop(){
+        running = false;
+        try{
+            this.thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        long timer = System.currentTimeMillis();
+        final double ns = 1000000000.0 / 60;
+        double delta = 0;
+        int frames = 0;
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                update();
+                delta--;
+            }
+            render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                this.frame.setTitle(title + " | " + frames + "fps" );
+                frames = 0;
+            }
+        }
+
+        stop();
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, WIDTH * 2 , HEIGHT * 2);
+
+        g.dispose();
+        bs.show();
+    }
+
+    private void update(){
+
+    }
+}
+
